2aa1f66d9ce6a5ff4f1b57e40196bb4931e705fb
silverberg89 <42668862+silverberg89@users.noreply.github.com>
2018-12-16 16:13:16 +0100

    Delete LUD.py

--- LUD.py
+++
@@ -1,7 +0,0 @@
-def LUD(M,f):
-    start = time.clock()
-    LU = linalg.lu_factor(M)
-    x = linalg.lu_solve(LU,f)
-    end = time.clock()
-    print ('CPU time (LUD case):',end-start)
-    return(x)
